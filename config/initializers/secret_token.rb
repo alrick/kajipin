f2ae64002e633e970a23debb0a6a2bc079defb4f
Alrick Deillon <alrick.deillon@gmail.com>
2012-06-04 15:01:53 +0200

    RoR Skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Socialrails::Application.config.secret_token = '4148b644dacc20f9a6187969b711f5b25c53ea7628ce42f75805288c9fd1871b6b6c23286ecbcef17676daf0db24373554559ad6672c0c609da83fb22d45919f'
