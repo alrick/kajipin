f4c96aa031fb5576c845ac5af58d39772db0f8e6
Alrick Deillon <alrick.deillon@gmail.com>
2012-07-27 17:43:58 +0200

    Devise and styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Socialrails::Application.routes.draw do
-  devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}
+  devise_for :users, path_names: {sign_in: "login", sign_out: "logout", sign_up: "join"}
 
   resources :assignments
 
