d807baebab1c5075d94bfeda193d5c30e98efc9b
Alrick Deillon <alrick.deillon@gmail.com>
2012-07-31 16:24:30 +0200

    Add gravatar for avatar management

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def gravatar_url(user, size)
+    gravatar_hash = Digest::MD5.hexdigest(user.email.downcase)
+    # s = size, d = default avatar, r = rating (g for any audience type)
+    "http://gravatar.com/avatar/#{gravatar_hash}?s=#{size}&d=mm&r=g"
+  end
 end
