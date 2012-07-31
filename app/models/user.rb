6fc49669ddecd141b82b48a95d63582940980eb1
Alrick Deillon <alrick.deillon@gmail.com>
2012-07-31 09:23:36 +0200

    Add devise confirmable module and terms in register

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,17 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
-  attr_accessible :email, :first_name, :last_name, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :first_name, :last_name, :password, :password_confirmation, :remember_me, :terms
 
   has_many :locations, dependent: :destroy
   has_many :accesstokens, dependent: :destroy
   has_many :comments
   has_many :friendships, dependent: :destroy
   has_many :friends, :through => :friendships
+
+  validates :first_name, :length => { :minimum => 2 }
+  validates :last_name, :length => { :minimum => 2 }
+  validates :terms, :acceptance => true
 end
