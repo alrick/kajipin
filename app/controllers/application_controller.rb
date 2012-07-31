02959591644dcdcc288bc3e2bcafe49aba2c152d
Alrick Deillon <alrick.deillon@gmail.com>
2012-07-31 09:22:29 +0200

    Add redirect after sign_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  def after_sign_in_path_for(resource)
+   tags_path
+  end
+
   def after_sign_out_path_for(resource_or_scope)
     new_session_path(resource_name)
   end
