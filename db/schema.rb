e4ebc9558aef448b4116db9714578a4da9f170f7
Alrick Deillon <alrick.deillon@gmail.com>
2012-07-31 09:28:06 +0200

    Add devise confirmable

--- db/schema.rb
+++ db/schema.rb
@@ -107,6 +107,10 @@ ActiveRecord::Schema.define(:version => 20120724065650) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
