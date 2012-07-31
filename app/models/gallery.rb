710586098a7b835b29125539e7f39c441ffa3e40
Alrick Deillon <alrick.deillon@gmail.com>
2012-07-31 09:31:16 +0200

    Change gallery to test simple form

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,6 +1,8 @@
 class Gallery < ActiveRecord::Base
+  attr_accessible :name, :location_id
+
   belongs_to :location
-  
+
   validates :name, :presence => true
   validates :name, :length => { :maximum => 100 }
   validates :location_id, :numericality => { :greater_than => 0}
