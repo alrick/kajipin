10c489d566e979103dca39603db4aecc51d9cefb
Alrick Deillon <alrick.deillon@gmail.com>
2012-08-13 14:56:44 +0200

    Default user is current user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,10 @@ class UsersController < ApplicationController
   before_filter :authenticate_user!
 
   def show
-    @user = User.includes(:locations).find(params[:id])
+    if params[:id].nil?
+      @user = User.includes(:locations).find(current_user)
+    else
+      @user = User.includes(:locations).find(params[:id])
+    end
   end
 end
