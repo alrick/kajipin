e1a2d3501832ef4d96f8e27dc72c5e84012dca6b
Alrick Deillon <alrick.deillon@gmail.com>
2012-08-13 14:57:03 +0200

    Extends user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ActiveRecord::Base
+  before_validation :default_values
+
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
-  attr_accessible :email, :first_name, :last_name, :password, :password_confirmation, :remember_me, :terms
+  attr_accessible :email, :first_name, :last_name, :password, :password_confirmation, :remember_me, :terms, :birth, :sex
 
   has_many :locations, dependent: :destroy
   has_many :accesstokens, dependent: :destroy
@@ -15,4 +17,15 @@ class User < ActiveRecord::Base
   validates :first_name, :length => { :minimum => 2 }
   validates :last_name, :length => { :minimum => 2 }
   validates :terms, :acceptance => true
+
+  def default_values
+    
+  end
+
+  def age
+    if(!birth.nil?)
+      age = Date.today.year - birth.year
+      age -= 1 if Date.today < birth + age.years #for days before birthday
+    end
+  end
 end
