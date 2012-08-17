334247e9923e241325f66cabc4c7d1498af259e3
Alrick Deillon <alrick.deillon@gmail.com>
2012-08-17 17:29:26 +0200

    Add single location localisation

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -29,6 +29,12 @@ class LocationsController < ApplicationController
 
   def index
     gon.rabl "app/views/locations/index.json.rabl", as: "locations"
+
+    # Get location if map opened via a single location
+    if !params[:l].nil?
+      gon.highloc = params[:l]
+    end
+
     render :layout => 'map'
   end
 
@@ -38,7 +44,14 @@ class LocationsController < ApplicationController
 
   def new
     @location = Location.new
-    url = "http://api.geonames.org/searchJSON?q=london&maxRows="+Geonamesconfig.maxrows+"&fuzzy="+Geonamesconfig.fuzzy+"&username="+Geonamesconfig.username
+
+    if params[:q].nil?
+      q = "london"
+    else
+      q = URI.escape(params[:q])
+    end
+
+    url = "http://api.geonames.org/searchJSON?q="+q+"&maxRows="+Geonamesconfig.maxrows+"&fuzzy="+Geonamesconfig.fuzzy+"&username="+Geonamesconfig.username
     @geos = JSON.parse(open(url).read)
   end
 
@@ -51,18 +64,23 @@ class LocationsController < ApplicationController
   end
 
   def create
-    @location = Location.new(params[:location])
+    location = current_user.locations.create
+    location.title = params[:title]
+    location.latitude = params[:latitude]
+    location.longitude = params[:longitude]
+    location.country_name = params[:country_name]
+    location.country_code = params[:country_code]
 
-    if @location.save
-      redirect_to @location, notice: 'Location was successfully created.'
+    if location.save
+      redirect_to manage_user_locations_url(current_user), notice: '<strong>Well done!</strong> Location was successfully added.'
     else
-      render action: "new"
+      redirect_to manage_user_locations_url(current_user), alert: '<strong>Oh snap!</strong> Something went wrong.'
     end
   end
 
   def update
     if @location.update_attributes(params[:location])
-      redirect_to @location, notice: 'Location was successfully updated.'
+      redirect_to @location, notice: '<strong>Well done!</strong> Location was successfully updated.'
     else
       render action: "edit"
     end
@@ -71,6 +89,6 @@ class LocationsController < ApplicationController
   def destroy
     @location.destroy
 
-    redirect_to locations_url
+    redirect_to manage_user_locations_url(current_user), notice: '<strong>Well done!</strong> Location was successfully removed.'
   end
 end
