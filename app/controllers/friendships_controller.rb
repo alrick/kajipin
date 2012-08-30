39a839c1161fbd95a9ecfaca526d75e01f7be931
Alrick Deillon <alrick.deillon@gmail.com>
2012-08-30 16:44:42 +0200

    Friendships functionality

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,85 +1,30 @@
 class FriendshipsController < ApplicationController
-  # GET /friendships
-  # GET /friendships.json
+
   def index
     @friendships = Friendship.all
-    @watchers = Friendship.where(:user_id => 2)
-    @sharers = Friendship.where(:friend_id => 2)
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @friendships }
-    end
+    @watcherships = Friendship.where(:user_id => current_user.id)
+    @sharerships = Friendship.where(:friend_id => current_user.id)
   end
 
-  # GET /friendships/1
-  # GET /friendships/1.json
-  def show
-    @friendship = Friendship.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @friendship }
-    end
-  end
-
-  # GET /friendships/new
-  # GET /friendships/new.json
-  def new
-    @friendship = Friendship.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @friendship }
-    end
-  end
-
-  # GET /friendships/1/edit
-  def edit
-    @friendship = Friendship.find(params[:id])
-  end
-
-  # POST /friendships
-  # POST /friendships.json
   def create
-    @friendship = Friendship.new(params[:friendship])
 
-    respond_to do |format|
+    if !current_user.isWatcher(params[:friend_id])
+      @friendship = current_user.friendships.build(:friend_id => params[:friend_id])
+
       if @friendship.save
-        format.html { redirect_to @friendship, notice: 'Friendship was successfully created.' }
-        format.json { render json: @friendship, status: :created, location: @friendship }
+        redirect_to friendships_path, notice: '<strong>Well done!</strong> Watcher added.'
       else
-        format.html { render action: "new" }
-        format.json { render json: @friendship.errors, status: :unprocessable_entity }
+        redirect_to friendships_path, alert: '<strong>Oh snap!</strong> Unable to add watcher.'
       end
+    else
+      redirect_to friendships_path, alert: '<strong>Oh snap!</strong> User is already your watcher.'
     end
   end
 
-  # PUT /friendships/1
-  # PUT /friendships/1.json
-  def update
-    @friendship = Friendship.find(params[:id])
-
-    respond_to do |format|
-      if @friendship.update_attributes(params[:friendship])
-        format.html { redirect_to @friendship, notice: 'Friendship was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @friendship.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /friendships/1
-  # DELETE /friendships/1.json
   def destroy
-    @friendship = Friendship.find(params[:id])
+    @friendship = current_user.friendships.find(params[:id])
     @friendship.destroy
 
-    respond_to do |format|
-      format.html { redirect_to friendships_url }
-      format.json { head :no_content }
-    end
+    redirect_to friendships_path, notice: '<strong>Well done!</strong> Successfully removed watcher.'
   end
 end
--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,6 @@
 class Friendship < ActiveRecord::Base
+  attr_accessible :user_id, :friend_id
+
   belongs_to :user
   belongs_to :friend, :class_name => "User"
   
