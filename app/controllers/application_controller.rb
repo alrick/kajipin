8326c6ad47da25e95d1e1a4f91452c765d6e6d62
Alrick Deillon <alrick.deillon@gmail.com>
2012-08-30 16:43:59 +0200

    Change redirect after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def after_sign_in_path_for(resource)
-   tags_path
+   user_path(current_user)
   end
 
   def after_sign_out_path_for(resource_or_scope)
