61f2443922065798eb2bb16a6bf6f95f5ca8ec7b
Alrick Deillon <alrick.deillon@gmail.com>
2012-09-17 17:09:55 +0200

    Heroku success

--- config/application.rb
+++ config/application.rb
@@ -59,7 +59,7 @@ module Socialrails
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    # Heroku requirement
+    # Heroku requirement 
     config.assets.initialize_on_precompile = false
   end
 end
