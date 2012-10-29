068f4d90c120f2f60375d442a66b46b2a3b78dc6
Alrick Deillon <alrick.deillon@gmail.com>
2012-10-29 10:59:39 +0100

    Add scopes for accesstokens and uniqueness

--- app/models/accesstoken.rb
+++ app/models/accesstoken.rb
@@ -3,5 +3,10 @@ class Accesstoken < ActiveRecord::Base
   
   validates :value, :presence => true
   validates :user_id, :numericality => { :greater_than => 0}
-  validates :type, :inclusion => { :in => %w(twitter facebook flickr dropbox) }
+  validates :ilk, :inclusion => { :in => %w(twitter facebook dropbox) }
+  validates :ilk, :uniqueness => {:scope => :user_id}
+
+  scope :dropbox, where(ilk: Appetizer.dropbox_token)
+  scope :twitter, where(ilk: Appetizer.twitter_token)
+  scope :facebook, where(ilk: Appetizer.facebook_token)
 end
