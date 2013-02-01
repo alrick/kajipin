bd9b66fb90309cccb091771b85c79fa5150c1a2b
Alrick Deillon <alrick.deillon@gmail.com>
2013-02-01 14:22:35 +0100

    Set specific routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,7 @@
 Kajipin::Application.routes.draw do
   devise_for :users, :path => "", path_names: {sign_in: "login", sign_out: "logout", sign_up: "join"}
 
-  resources :friendships
-
-  resources :pins do
-    collection do
-      get 'list'
-    end
-    resources :comments
-    resources :logpages, :path => 'logbook' do
-      collection do
-        get 'captain'
-      end
-    end
-  end
-
-  resources :users
+  resources :users, :only => [:index, :show]
 
   authenticated :user do
     root :to => "users#show"
@@ -25,6 +11,20 @@ Kajipin::Application.routes.draw do
       get "/" => "devise/sessions#new"
     end
   end
+
+  resources :pins do
+    collection do
+      get 'list'
+    end
+    resources :comments, :only => [:index, :create, :destroy]
+    resources :logpages, :only => [:index, :create, :update, :destroy], :path => 'logbook' do
+      collection do
+        get 'captain'
+      end
+    end
+  end
+
+  resources :friendships, :only => [:index, :create, :destroy]
   
   match "friends" => "friendships#index"
 
