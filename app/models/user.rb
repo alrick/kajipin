2fc31690e2c3500e75b629a494c0081164280e4e
Alrick Deillon <alrick.deillon@gmail.com>
2013-02-11 09:34:49 +0100

    Destroying a user remove his inverse friendships too

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ActiveRecord::Base
   has_many :comments
   has_many :friendships, dependent: :destroy
   has_many :friends, :through => :friendships
-  has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
+  has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id", dependent: :destroy
   has_many :inverse_friends, :through => :inverse_friendships, :source => :user
 
   validates :first_name, :length => { :minimum => 2 }
