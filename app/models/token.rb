268289fc9d7c45453c0de494b039d0c67cb52152
Alrick Deillon <alrick.deillon@gmail.com>
2013-02-19 17:00:18 +0100

    Add token model

---
+++ app/models/token.rb
@@ -0,0 +1,3 @@
+class Token < ActiveRecord::Base
+  attr_accessible :value
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130211131110) do
+ActiveRecord::Schema.define(:version => 20130219155633) do
 
   create_table "assignments", :force => true do |t|
     t.integer  "logpage_id"
@@ -83,6 +83,12 @@ ActiveRecord::Schema.define(:version => 20130211131110) do
     t.text     "description"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+  end
+
+  create_table "tokens", :force => true do |t|
+    t.string   "value"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "users", :force => true do |t|
