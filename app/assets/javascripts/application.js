bec98007916bde57c2c6df70e4398fdd37ebe5bf
Alrick Deillon <alrick.deillon@gmail.com>
2013-02-20 17:46:47 +0100

    Reorder assets in application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery
 //= require jquery-ui
 //= require jquery_ujs
+//= require jquery
 //= require twitter/bootstrap
 //= require mapbox/mapbox
 //= require hoverintent/jquery.hoverIntent.minified
