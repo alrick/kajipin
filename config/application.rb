f5c8f6229a7ae7a29a3419d95aa9b5fee7e63bb8
Alrick Deillon <alrick.deillon@gmail.com>
2013-02-20 16:48:19 +0100

    Fix problem with assets on heroku bis

--- config/application.rb
+++ config/application.rb
@@ -60,7 +60,7 @@ module Kajipin
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    # Heroku requirement 
+    # Heroku requirement
     config.assets.initialize_on_precompile = false
   end
 end
