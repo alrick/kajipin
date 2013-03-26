88054e873e44a933478f87864a729c9e7c50a4fc
Alrick Deillon <alrick.deillon@gmail.com>
2013-03-26 10:55:36 +0100

    Define validation for photos

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,19 +1,30 @@
 class Photo < ActiveRecord::Base
-  LIMIT = 3
+  include HTTParty # Used to access filepicker.io
+
+  LIMIT = 15
 
   attr_accessible :pin_id, :url
 
   belongs_to :pin
+
+  before_destroy :delete_s3
   
   validates :url, :format => URI::regexp(%w(https))
   validates :pin_id, :numericality => { :greater_than => 0 }
 
   validates_each :pin do |photo, attr, value|
-    photo.errors.add attr, "You've reached your photos limit for this pin" if photo.pin.photos.size >= LIMIT
+    photo.errors.add attr, "you've reached your photos limit for this pin" if photo.pin.photos.size >= LIMIT
   end
 
   # Pins limit getter
   def self.limit
     LIMIT
   end
+
+  # Delete S3 file
+  def delete_s3
+    try = self.class.delete(url)
+    try.success?
+  end
+
 end
