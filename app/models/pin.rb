2f9000a60a2904182a4b8a9e1e281cff12876261
Alrick Deillon <alrick.deillon@gmail.com>
2013-04-02 15:14:32 +0200

    Changes for new error messages

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -13,11 +13,11 @@ class Pin < ActiveRecord::Base
   validates :latitude, :longitude, :numericality => true
   validates :user_id, :ext_id, :numericality => { :greater_than => 0}
 
-  validates :user_id, :uniqueness => { :scope => [:latitude, :longitude], :message => "Your pin can't have same latitude and longitude than another pin" }
+  validates :user_id, :uniqueness => { :scope => [:latitude, :longitude], :message => "Pins can't have same latitude and longitude" }
 
   validates_each :user do |pin, attr, value|
-    pin.errors.add attr, "you've reached your pins limit" if pin.user.pins.count >= LIMIT
-    pin.errors.add attr, "you've reached your high populated pins limit" if pin.high_populated? && pin.user.pins.high_populated.count >= HIGH_POPULATED_LIMIT
+    pin.errors.add attr, "Pins limit reached" if pin.user.pins.count >= LIMIT
+    pin.errors.add attr, "High populated pins limit reached" if pin.high_populated? && pin.user.pins.high_populated.count >= HIGH_POPULATED_LIMIT
   end
 
   scope :bigcity, where(:type => "Bigcity")
