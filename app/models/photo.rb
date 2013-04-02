8985e5ca9d8cb64b4327b953e30090392da92d40
Alrick Deillon <alrick.deillon@gmail.com>
2013-04-02 16:29:25 +0200

    Use custom methods validation

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,6 +2,7 @@ class Photo < ActiveRecord::Base
   include HTTParty # Used to access filepicker.io
 
   LIMIT = 50
+  GLOBAL_LIMIT = 25000
 
   attr_accessible :pin_id, :url, :key, :handle, :size
 
@@ -12,10 +13,8 @@ class Photo < ActiveRecord::Base
   validates :url, :format => URI::regexp(%w(https))
   validates :pin_id, :numericality => { :greater_than => 0 }
   validates :key, :handle, :presence => true
-
-  validates_each :pin do |photo, attr, value|
-    photo.errors.add attr, "you've reached your photos limit for this pin" if photo.pin.photos.size >= LIMIT
-  end
+  validate :check_global_limit, :on => :create
+  validate :check_limit, :on => :create
 
   # Pins limit getter
   def self.limit
@@ -38,6 +37,20 @@ class Photo < ActiveRecord::Base
   end
 
   private
+
+  # Check limit isn't reached
+  def check_limit
+    if pin.photos.count >= LIMIT
+      errors[:base] << "Photos limit reached for this pin"
+    end
+  end
+
+  # Check global limit isn't reached
+  def check_global_limit
+    if Photo.count >= GLOBAL_LIMIT
+      errors[:base] << "Photos overloading, please contact support"
+    end
+  end
 
   # Get filepicker policy
   def get_policy(call)
