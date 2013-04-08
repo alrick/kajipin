3d2f4cd62e73379d1862a459857aefc788608c39
Alrick Deillon <alrick.deillon@gmail.com>
2013-04-08 15:26:33 +0200

    Create filepicker class to implement filepicker specific functions

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,4 @@
-class Photo < ActiveRecord::Base
-  include HTTParty # Used to access filepicker.io
+class Photo < Filepicker
 
   LIMIT = 50
   GLOBAL_LIMIT = 25000
@@ -21,21 +20,6 @@ class Photo < ActiveRecord::Base
     LIMIT
   end
 
-  # Delete S3 file
-  def delete_remote
-    policy = get_policy("remove")
-    signature = get_signature(policy)
-    remote = url+"?signature="+signature+"&policy="+policy
-    try = self.class.delete(remote)
-
-    # If file not found in filepicker, destroy anyway, else only if success
-    if try.not_found?
-      true
-    else
-      try.success?
-    end
-  end
-
   private
 
   # Check limit isn't reached
@@ -50,23 +34,6 @@ class Photo < ActiveRecord::Base
     if Photo.count >= GLOBAL_LIMIT
       errors[:base] << "Photos overloading, please contact support"
     end
-  end
-
-  # Get filepicker policy
-  def get_policy(call)
-    if !["pick", "read", "stat", "write", "writeUrl", "store", "convert", "remove"].include? call
-      raise "Incorrect call"
-    else
-      expiry = Time.now.to_i + 60*60
-      json_policy = {:call => call, :handle => handle, :expiry => expiry}.to_json
-      policy = Base64.urlsafe_encode64(json_policy)
-    end
-  end
-
-  # Get filepicker signature
-  def get_signature(policy)
-    secret = ENV["FILEPICKER_SECRET"]
-    signature = OpenSSL::HMAC.hexdigest("sha256", secret, policy)
   end
 
 end
