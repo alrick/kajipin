77b477f6b9334263470daa951761fc75b81c7a63
Alrick Deillon <alrick.deillon@gmail.com>
2013-04-08 15:13:25 +0200

    Add validations to avatar

--- app/models/avatar.rb
+++ app/models/avatar.rb
@@ -1,5 +1,12 @@
-class Avatar < ActiveRecord::Base
+class Avatar < Filepicker
   attr_accessible :handle, :key, :size, :url, :user_id
 
   belongs_to :user
+
+  before_destroy :delete_remote
+
+  validates :url, :format => URI::regexp(%w(https))
+  validates :user_id, :numericality => { :greater_than => 0 }
+  validates :key, :handle, :presence => true
+
 end
