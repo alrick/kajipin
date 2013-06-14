7a2ced2af3ba55bb10eee6edf8f892aa5e31de25
Alrick Deillon <alrick.deillon@gmail.com>
2013-06-14 15:12:00 +0200

    Fix bootstrap upgrade

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,6 @@
 // GO AFTER THE REQUIRES BELOW.
 //
 //= require jquery
-//= require jquery-ui
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require hoverintent/jquery.hoverIntent.minified
