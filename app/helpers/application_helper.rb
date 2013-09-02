9ccdea9bceff38af172d23027dbb800a38fd226d
Alrick Deillon <alrick.deillon@gmail.com>
2013-09-02 17:14:43 +0200

    Add share request helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
   def sharing_btn(user, size)
     if @herald.nil? # if isn't accessed via herald
       if user == current_user
-        link_to "That's you!", "#", :class => "btn btn-social #{size}", :rel => "tooltip", :data => { :title => "Yeah really..", :placement => "left" }
+        link_to "That's you!", "#", :class => "btn btn-social #{size}", :rel => "tooltip", :data => { :title => "Yeah really..", :placement => "top" }
       else
         if current_user.isSharingWith(user)
           friendship = current_user.friendships.where(:friend_id => user.id).first
@@ -20,6 +20,14 @@ module ApplicationHelper
       content_tag(:span, "Sharer", :class => "label label-social is-sharer")
     else
       content_tag(:span, "&nbsp;".html_safe)
+    end
+  end
+
+  def share_request(custom_classes)
+    if Request.where(:user_id => @user.id, :requester_id => current_user.id).exists?
+      link_to "Request sent", "#", :class => "btn btn-social disabled #{custom_classes}"
+    else
+      link_to "Request", requests_path(:user => @user, :requester => current_user), :method => :post, :class => "btn btn-social #{custom_classes}"
     end
   end
 
